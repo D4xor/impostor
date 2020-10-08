c98944b887da842f9192aa6c1f6336822512e869
Maxime <doreaumaxime@gmail.com>
2020-10-08 20:10:59 +0200

    Add new functions in partidas

--- cliente/modelo.js
+++ cliente/modelo.js
@@ -10,13 +10,16 @@ function Juego(){
 		//crear el objeto partida: num  owner
 		//this.partidas[codigo] = nueva partida
 	}
-	this.unirAPartida=function(nick){
-		//TODO
+	this.unirAPartida=function(codigo,nick){
+		if (this.partidas[codigo]) {
+			this.partidas[codigo].agregarUsuario(nick);
+		}
 	}
 
 	this.obtenerCodigo=function(){
 		let cadena = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
 		let letras = cadena.split('');
+		let maxCadena=cadena.length;
 		let codigo = [];
 
 		for(i=0;i<6;i++){
@@ -29,15 +32,44 @@ function Juego(){
 
 function Partida(num,owner){
 	this.numUsuarios=num;
-	this.owner=owner;
-	this.usuarios=[]; //el index 0 sera el owner
-	this.agregarUsuario=function(nick){
-		//comprobar nick unico
-		//comprobar si el usuario num
-		//TODO
+	this.nickOwner=owner;
+	this.fase=new Inicial();
+	this.usuarios={}; //el index 0 sera el owner
+	this.agregarUsuario=function(){
+		this.fase.agregarUsuario(nick,this);
 	}
-
+	this.puedeAgregarUsuario=function(nick){
+		let nuevo=nick;
+		let contador=1;
+		while(this.usuarios[nuevo]){
+			nuevo=nick+contador;
+			contador=contador+1;
+		}
+		this.usuarios[nuevo]=new Usuario(nuevo);
+	}
 	this.agregarUsuario(owner);
+}
+
+function Inicial(){
+	this.agregarUsuario=function(nick,partida){
+		partida.puedeAgregarUsuario(nick);
+	}
+}
+
+function Jugando(){
+	this.agregarUsuario=function(nick,partida){
+		//this.puedeAgregarUsuario(nick);
+	}
+}
+
+function Final(){
+	this.agregarUsuario=function(nick,partida){
+		//this.puedeAgregarUsuario(nick);
+	}
+}
+
+function Usuario(nick){
+	this.nick=nick;
 }
 
 
