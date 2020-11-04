78846078817c1b101229da1b1c3f79acb016333b
Maxime <doreaumaxime@gmail.com>
2020-11-04 17:34:26 +0100

    updates

--- cliente/src/clienteRest.js
+++ cliente/src/clienteRest.js
@@ -1,3 +1,17 @@
-function ClienteRest(){
-
+function clienteRest(){
+	this.crearPartida=function(nick,num){
+		$.getJSON("/crearPartida"+nick+"/"+num,function(data){
+			console.log(data);
+		});
+	}
+	this.unirAPartida=function(nick,codigo){
+		$.getJSON("/unirAPartida"+nick+"/"+codigo,function(data){
+			console.log(data);
+		});
+	}
+	this.listaPartidas=function(){
+		$.getJSON("/listaPartidas",function(data){
+			console.log(data);
+		});
+	}
 }
